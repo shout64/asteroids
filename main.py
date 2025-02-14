6687ed86bcbd4b33cb5fca5e73cb69bc6f4a1001
Devin Chaney <shout64@gmail.com>
2025-02-14 13:43:22 -0600

    Update game loop with delta time

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ def main():
     print("Starting asteroids!")
     pygame.init()
 
+    dt = 0  
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
     # Game loop
     while True:
@@ -20,6 +23,8 @@ def main():
 
 
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        # dt = x / 1000
 
 
 
