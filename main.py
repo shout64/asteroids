45f497404faaf00cf98342420c9470d275fb1497
Devin Chaney <shout64@gmail.com>
2025-02-15 14:45:10 -0600

    Create player class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import * 
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -11,6 +12,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius=PLAYER_RADIUS)
 
     # Game loop
     while True:
@@ -19,8 +21,8 @@ def main():
                 return
 
         screen.fill("black")
-
-
+        
+        player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60)/1000
